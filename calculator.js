6046fd1558ff53d6fbf56f6d74d60bb223eba1cf
Raj <r.patel823@yahoo.com>
2025-01-29 10:54:47 -0500

    Initial files creation
    Made starter files with basic calc operations

---
+++ calculator.js
@@ -0,0 +1,53 @@
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function divide(num1, num2)
+{
+    return num1 / num2;
+}
+
+//calls function based on given operand
+function operate(num1, num2, operand)
+{
+    if (operand === "+")
+    {
+        return add(num1, num2);
+    }
+
+    else if (operand === "-")
+    {
+        return subtract(num1, num2);
+    }
+
+    else if (operand === "*")
+    {
+        return multiply(num1, num2);
+    }
+
+    else
+    {
+        return divide(num1, num2);
+    }
+}
+
+//represent each part of an operation
+let num1 = 10, num2 = 2, operand = "*";
+
+console.log(operate(num1, num2, operand))
+
+
+
+
+
